319dd09faf6c4df76db8e5e87090f25905851a31
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2025-12-29 00:29:45 +0200

    Finsh-users controller.

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -3,13 +3,31 @@ const DetaledError = require('../utils/detaledError.utils');
 
 module.exports = {
     getAll: async (req, res) => {
-        return UsersService.getAll();
+        res.status(200).json({ message: "OK", result: UsersService.getAll() });
     },
     create: async (req, res) => {
         const isCreated = UsersService.create(req.body);
 
         if (!isCreated) {
-            throw new DetaledError
+            throw new DetaledError('SERVER_ERROR', "FAILD TO CREATE!")
+        }
+
+        res.status(201).json({ message: "OK" })
+    },
+    update: async (req, res) => {
+        const isUpdated = UsersService.update(req.body, req.params.id);
+
+        if (!isUpdated) {
+            throw new DetaledError('SERVER_ERROR', "FAILD TO UPDATE!")
+        }
+
+        res.status(204).json({ message: "OK" });
+    },
+    delete: async (req, res) => {
+        const isDeleted = UsersService.delete(req.params.id);
+
+        if (!isDeleted) {
+            throw new DetaledError('SERVER_ERROR', "FAILD TO DELETE!")
         }
     }
 }
