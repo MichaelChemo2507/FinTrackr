ce417f636a1c4c7018debcdd26e62f4ea7fb9a53
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2026-01-26 08:04:20 +0200

    Finish-Finished the registration welcome paragraph.

--- client/src/content/registrationPage.js
+++ client/src/content/registrationPage.js
@@ -2,9 +2,9 @@ const registrationWelcomeText = {
     title: "Get a clear financial picture",
     body: "Track your income,expenses, and savinfs in one place.\n FinTrakr helps you understand where your money goes and build better financial habits.",
     features: [
-        {icon:"/lighbulb_icon-Large.png",title:"Smart Insights",text:"Get a clear, real-time view of your data with intelligent analytics"},
-        {icon:"/deadline_icon-Large.png",title:"Faster Decisions",text:"Track trends, detect anomalies, and act with confidence"},
-        {icon:"/controlling_icon-Large.png",title:"Stay In Control",text:"Receive smart alerts and stay ahead at all times"},
+        {icon:"images/lighbulb_icon-Large.png",title:"Smart Insights",text:"Get a clear, real-time view of your data with intelligent analytics"},
+        {icon:"images/deadline_icon-Large.png",title:"Faster Decisions",text:"Track trends, detect anomalies, and act with confidence"},
+        {icon:"images/controlling_icon-Large.png",title:"Stay In Control",text:"Receive smart alerts and stay ahead at all times"},
     ]
     
 }
