ffcbc6fa3331ae4e06b0370e85187259489015bb
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2026-02-26 20:23:14 +0200

    Add-login-process change error result format.

--- server/middleware/errorHandler.middleware.js
+++ server/middleware/errorHandler.middleware.js
@@ -6,10 +6,13 @@ module.exports = (error, req, res, next) => { // basic error handler.
     if (Object.values(STATUS_CODES).includes(error.status)) {
 
         if (error.status != STATUS_CODES.SERVER_ERROR) {
-            return res.status(error.status).send(error.message);
+            return res.status(error.status).json({
+                success: false,
+                message: error.message
+            });
         }
         
     }
 
-    return res.status(500).send("Something whent worng please try again later!");
+    return res.status(500).json({ success: false, message: "Something whent worng please try again later!" });
 }
