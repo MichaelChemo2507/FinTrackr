d1200dee476b2fc18f35f1a5be75126a252062d2
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2026-02-26 20:28:58 +0200

    Add-login-process add login api call, fix the cookie managment.

--- client/src/handlers/onSubmit.jsx
+++ client/src/handlers/onSubmit.jsx
@@ -2,7 +2,7 @@ import Urls from "../content/urls.js";
 
 const onSubmitMethods = {
     registration: async (abortControllerRef, formData) => {
-        
+
         abortControllerRef.current?.abort();
         abortControllerRef.current = new AbortController();
 
@@ -13,14 +13,37 @@ const onSubmitMethods = {
             headers: {
                 "Content-Type": "application/json",
             },
+            credentials: "include",
             body: JSON.stringify(formData),
 
         }
         );
-        
+
         const result = await response.json();
-        
-        console.log(result);
+
+        return result;
+
+    }, login: async (abortControllerRef, formData) => {
+
+        abortControllerRef.current?.abort();
+        abortControllerRef.current = new AbortController();
+
+        const response = await fetch(Urls.login, {
+
+            method: "POST",
+            signal: abortControllerRef.current.signal,
+            headers: {
+                "Content-Type": "application/json",
+            },
+            credentials: "include",
+            body: JSON.stringify(formData),
+
+        }
+        );
+
+        const result = await response.json();
+
+        return result;
 
     }
 }
