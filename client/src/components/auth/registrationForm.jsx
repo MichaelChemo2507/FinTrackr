6335024568c6c11da88f90cba2db0c8976dd8ab0
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2026-02-26 20:27:55 +0200

    Add-login-process fixing error display.

--- client/src/components/auth/registrationForm.jsx
+++ client/src/components/auth/registrationForm.jsx
@@ -1,58 +1,55 @@
-import { useState,useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import onSubmitMethods from "../../handlers/onSubmit.jsx";
+import { Link } from "react-router-dom";
+import Text from "../ui/text.jsx";
+import { useNavigate } from "react-router-dom";
 
 export default function RegistrationForm() {
 
     const [error, setError] = useState();
-    const [formData, setFormData] = useState({ 'userName': '', 'email': '', 'password': '', 'phone': '' });
-    const [isLoading, setIsLoading] = useState(false);
-    
+    const [formData, setFormData] = useState({ 'userName': { type: 'text', value: '' }, 'email': { type: 'email', value: '' }, 'password': { type: 'password', value: '' }, 'phone': { type: 'tel', value: '' } });
+    const navigate = useNavigate();
+
     const abortControllerRef = useRef(null);
 
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
-
-    if (error) {
-        return <div>Error: {error}</div>;
-    }
+    useEffect(() => {
+        if (error) {
+            alert(error.message);
+        }
+    }, [error]);
 
     return (
         <div>
-            <form onSubmit={(e) => {
+            <form onSubmit={async (e) => {
 
                 e.preventDefault();
-                
-                setIsLoading(true);
-                
+
                 try {
-                    onSubmitMethods.registration(abortControllerRef,formData);
-                } catch (e) {
+                    const formDataToSend = Object.fromEntries(Object.keys(formData).map(key => [key, formData[key].value]));
 
-                    if (e.name === 'AbortError') { 
-                        
-                        console.log('Request was aborted');
+                    const result = await onSubmitMethods.registration(abortControllerRef, formDataToSend);
 
-                        return;
+                    if (result.success == false) {
+                        throw new Error(result.message);
                     }
 
+                    navigate('/login');
+
+                } catch (e) {
                     setError(e);
-                } finally {
-                    setIsLoading(false);
-                    //redirect to login page or dashboard after successful registration
                 }
             }}>
                 <h1>Create Account</h1>
-                <div>Start tracking your finances today</div>
+                <Text>Start tracking your finances today</Text>
 
-                {Object.keys(formData).map(key => <input type={key} value={formData[key]} placeholder={`${key}...`} onChange={e => {
-                    setFormData({ ...formData, [key]: e.target.value });
+                {Object.keys(formData).map(key => <input type={formData[key].type} value={formData[key].value} placeholder={`${key}...`} onChange={e => {
+                    setFormData({ ...formData, [key]: { ...formData[key], value: e.target.value } });
                 }} />
                 )}
 
                 <button type="submit">Get Started</button>
             </form>
-            <div>Already have an account?<a href="">Sign in</a></div> {/* Link to login page - not exist yet */}
+            <div>Already have an account?<Link to={'/login'}>Sign in</Link></div> {/* Link to login page - not exist yet */}
         </div>
     );
 }
